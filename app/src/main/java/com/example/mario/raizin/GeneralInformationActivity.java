59271e25d95e2e905064395816339a60dc37b3de
Nick Zombolas <nickzombolas@gmail.com>
2019-03-15 17:10:58 -0400

    shopping button added

--- app/src/main/java/com/example/mario/raizin/GeneralInformationActivity.java
+++ app/src/main/java/com/example/mario/raizin/GeneralInformationActivity.java
@@ -1,16 +1,28 @@
 package com.example.mario.raizin;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class GeneralInformationActivity extends AppCompatActivity {
 
+    Button shoppingButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        shoppingButton  = (Button)findViewById(R.id.shoppingButton);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_information);
+    }
 
+    public void clickMe(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.ca/s?k=sunscreen&ref=nb_sb_noss"));
+        startActivity(intent);
     }
 }
